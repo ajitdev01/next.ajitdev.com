d1ab16e3c7df13d55bdb23960de815f5f2737591
Ajit Rajput <ajitk23192@gmail.com>
2026-09-13 18:48:18 +0530

    just Try This Next Project's

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <h1>
+        Hello Just W8
+      </h1>
+    </div>
+    </>
+    
+  );
+}
